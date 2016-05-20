60499009d147437812f71b09a3357b2dd7594663
Ankur Chauhan <achauhan@inkriti.us>
2016-05-20 23:23:48 +0530

    Commit after adding uber-jar jar. Still getting same errors.

--- bundle/src/main/java/com/vfcorp/innovation/components/PreviewObject.java
+++ bundle/src/main/java/com/vfcorp/innovation/components/PreviewObject.java
@@ -1,20 +1,22 @@
 package com.vfcorp.innovation.components;
-
+import com.adobe.cq.commerce.api.Product;
 import com.adobe.granite.ui.components.AttrBuilder;
+import com.adobe.granite.ui.components.ComponentHelper;
+import com.adobe.granite.ui.components.Config;
 import com.day.cq.commons.date.RelativeTimeFormat;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
+import com.adobe.granite.ui.components.Tag;
 
 @Component(immediate = true,enabled = true)
 public class PreviewObject {
 
 
-    public static void getErrors(ResourceResolver resourceResolver, SlingHttpServletRequest slingRequest){
+    public static void getErrors(ComponentHelper cmp , ResourceResolver resourceResolver, SlingHttpServletRequest slingRequest){
 
-        ComponentHelper cmp = new ComponentHelper(pageContext);
         Config cfg = cmp.getConfig();
         String path = cmp.getExpressionHelper().getString(cfg.get("path", String.class));
         if (path == null) return;
