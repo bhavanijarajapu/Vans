f0f864786f7199ad6a313907578069e4b40db3b0
Ankur Chauhan <achauhan@inkriti.us>
2016-05-20 11:48:00 +0530

    Commit after adding dependencies not avalilabe on public repo.

--- bundle/src/main/java/com/vfcorp/innovation/components/PreviewObject.java
+++ bundle/src/main/java/com/vfcorp/innovation/components/PreviewObject.java
@@ -1,12 +1,18 @@
 package com.vfcorp.innovation.components;
 
+import com.adobe.granite.ui.components.AttrBuilder;
+import com.day.cq.commons.date.RelativeTimeFormat;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 
 @Component(immediate = true,enabled = true)
 public class PreviewObject {
 
 
-    public static void getErrors(){
+    public static void getErrors(ResourceResolver resourceResolver, SlingHttpServletRequest slingRequest){
 
         ComponentHelper cmp = new ComponentHelper(pageContext);
         Config cfg = cmp.getConfig();
@@ -20,6 +26,6 @@ public class PreviewObject {
         Tag tag = cmp.consumeTag();
         AttrBuilder attrs = tag.getAttrs();
         attrs.addClass("coral-ColumnView-column");
-        RelativeTimeFormat rtf = new RelativeTimeFormat("r", slingRequest.getResourceBundle(request.getLocale()));
+        RelativeTimeFormat rtf = new RelativeTimeFormat("r", slingRequest.getResourceBundle(slingRequest.getLocale()));
     }
 }
